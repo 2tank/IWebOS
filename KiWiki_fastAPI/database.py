c91023fb7b70fe82f6cb09ab7d786e04f79448b1
Pablo Márquez Benítez <pablomarquezbenitez78@gmail.com>
2024-10-28 18:56:44 +0100

    delete y update (por ahora)

--- KiWiki_fastAPI/database.py
+++ KiWiki_fastAPI/database.py
@@ -6,11 +6,31 @@ import motor.motor_asyncio
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+from bson import ObjectId
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='.env')
 
 MONGO_DETAILS = os.getenv("MONGO_URI")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-database = client.my_database   # Nombre de tu base de datos en MongoDB
-collection = database.my_collection   # Nombre de la colección
+database = client.IWebOS   # Nombre de tu base de datos en MongoDB
+entryCollection = database['Entry']   # Nombre de la colección
 #////////////////////////////////////////
+
+async def delete_entry_id(id: str):
+    deleted = False
+    entry = await entryCollection.find_one({"_id":ObjectId(id)})
+    if entry:
+        await entry.delete_one({"_id": ObjectId(id)})
+        deleted = True
+    return deleted 
+
+async def update_entry(id: str, data: dict):
+    if not data:
+        return False
+    entry = await entryCollection.find_one({"_id":ObjectId(id)})
+    if entry:
+        updatedEntry = await entryCollection.update_one({
+            {"_id": ObjectId(id)},{"$set":data}})
+        return bool(update_entry)
