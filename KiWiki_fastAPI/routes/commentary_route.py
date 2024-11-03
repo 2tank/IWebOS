1b67aec59f4faa8ead0525c6f079c1762d5bba9f
jaiRodRod <jaezro@uma.es>
2024-11-03 13:09:07 +0100

    mini correcciones

--- KiWiki_fastAPI/routes/commentary_route.py
+++ KiWiki_fastAPI/routes/commentary_route.py
@@ -35,7 +35,7 @@ async def get_commentary_number_of_responses(id: str):
     result = await commentary_logic.numberOfResponses(id)
     return result
 
-@router.get("/getResponses/{id}")
+@router.get("/commentaryResponses/{id}")
 async def get_commentary_get_responses(id: str):
     result = await commentary_logic.getResponses(id)
     return result
@@ -51,22 +51,22 @@ async def update_commentary(id: str, req: commentary = Body(...)):
     result = await commentary_logic.updateCommentary(id,req)
     return result
 
-@router.get("/getAllCommentariesInEntry/{id_entrada}")
+@router.get("/allCommentariesInEntry/{id_entrada}")
 async def get_commentaries_in_entry(id_entrada: str):
     result = await commentary_logic.getAllCommentariesFromEntry(id_entrada)
     return result
 
-@router.get("/getMainCommentariesInEntry/{id_entrada}")
+@router.get("/mainCommentariesInEntry/{id_entrada}")
 async def get_main_commentaries_in_entry(id_entrada: str):
     result = await commentary_logic.getMainCommentariesFromEntry(id_entrada)
     return result
 
-@router.get("/getAllCommentariesInEntrySpecificVersion/{id_entrada}/{id_version}")
+@router.get("/allCommentariesInEntrySpecificVersion/{id_entrada}/{id_version}")
 async def get_commentaries_in_entry_specific_version(id_entrada: str, id_version: str):
     result = await commentary_logic.getAllCommentariesFromEntrySpecificVersion(id_entrada, id_version)
     return result
 
-@router.get("/getMainCommentariesInEntrySpecificVersion/{id_entrada}/{id_version}")
+@router.get("/mainCommentariesInEntrySpecificVersion/{id_entrada}/{id_version}")
 async def get_main_commentaries_in_entry_specific_version(id_entrada: str, id_version: str):
     result = await commentary_logic.getMainCommentariesFromEntrySpecificVersion(id_entrada, id_version)
     return result
