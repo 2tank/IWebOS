1793842d50f265384cbebd05e1d02c14e25369ac
Pablo  Aguilar <135460549+pablo-972@users.noreply.github.com>
2024-11-04 11:42:54 +0100

    update/post wiki

--- KiWiki_fastAPI/item_logic/wiki.py
+++ KiWiki_fastAPI/item_logic/wiki.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Body
 from fastapi.encoders import jsonable_encoder
 
 from database import MONGOCRUD
-from models.wiki_model import WikiModel
+from models.wiki_schema import WikiSchema
 from datetime import date,datetime
 
 crud = MONGOCRUD('Wiki') 
@@ -11,3 +11,8 @@ crud = MONGOCRUD('Wiki')
 async def get_wikis():
     wikis = await crud.get_collection()
     return wikis
+
+
+async def post_wiki(entry):
+    entry_data = jsonable_encoder(entry)
+    result = await crud.create_item(entry_data)
--- KiWiki_fastAPI/routes/wiki_route.py
+++ KiWiki_fastAPI/routes/wiki_route.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Body
 from bson import ObjectId
 import item_logic.wiki as wiki_logic
+from models.wiki_schema import WikiSchema
 
 router = APIRouter()
 
@@ -11,5 +12,14 @@ async def get_wikis():
         return wikis
     except:
         raise HTTPException(status_code=500, detail="No wikis") 
+
+
+@router.post("/")
+async def post_wiki(entrt:WikiSchema = Body(...)):
+    try:
+        response = await wiki_logic.post_wiki()
+        return response
+    except:
+        raise HTTPException(status_code=500, detail="No wikis")
 
     
