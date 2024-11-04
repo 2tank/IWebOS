f6f18bc7179aa3a3b2e8506b3574518e3ac8e478
2tank <adrianfeve7@gmail.com>
2024-11-04 13:41:10 +0100

    cambios en wiki, para mas info te metes

--- KiWiki_fastAPI/models/wiki_schema.py
+++ KiWiki_fastAPI/models/wiki_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime #Ponemos el as DateType ya que si no entra en conflicto la línea 10 con el date y el Field
 from typing import List
 
@@ -8,4 +8,10 @@ class WikiSchema(BaseModel):
     creator: str = Field(..., max_length=20, description="Nombre del creador de la wiki")
     description: str = Field(..., max_length=50, description="Decripción de la wiki")
     date: datetime = Field(..., description="Fecha de creación de la wiki")
-    entries: List[str] = Field(None, description="Lista de entradas asociadas")
+    entries: List[str] = Field(default_factory=list, description="Lista de entradas asociadas")
+
+    @field_validator('entries', mode='before')
+    def validate_entries(cls, v):
+        if v is not None and len(v) > 0:
+            raise ValueError('Entries should not be provided on creation and must be empty.')
+        return v
--- KiWiki_fastAPI/routes/wiki_route.py
+++ KiWiki_fastAPI/routes/wiki_route.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, HTTPException, Body
-from bson import ObjectId
 import item_logic.wiki as wiki_logic
 from models.wiki_schema import WikiSchema
 
@@ -10,16 +9,47 @@ async def get_wikis():
     try:
         wikis = await wiki_logic.get_wikis()
         return wikis
-    except:
+    except Exception as e:
+        print(f"Se produjo un error: {e}")  # Imprime el error para el diagnóstico
         raise HTTPException(status_code=500, detail="No wikis") 
 
 
 @router.post("/")
 async def post_wiki(entrt:WikiSchema = Body(...)):
     try:
-        response = await wiki_logic.post_wiki()
+        response = await wiki_logic.post_wiki(entrt)
         return response
-    except:
+    except Exception as e:
+        print(f"Se produjo un error: {e}")  # Imprime el error para el diagnóstico
         raise HTTPException(status_code=500, detail="No wikis")
-
     
+
+@router.get("/entries_name")
+async def get_entries(content: str):
+    try:
+        response = await wiki_logic.get_entries_name(content)
+        return response
+    except Exception as e:
+        print(f"Se produjo un error: {e}")  # Imprime el error para el diagnóstico
+        raise HTTPException(status_code=400, detail="No entries for this wiki")
+
+
+@router.get("/entries_id")
+async def get_entries_id(content: str):
+    try:
+        response = await wiki_logic.get_entries_id(content)
+        return response
+    except Exception as e:
+        print(f"Se produjo un error: {e}")  # Imprime el error para el diagnóstico
+        raise HTTPException(status_code=400, detail="No entries for this wiki")
+    
+
+@router.patch("/{id_wiki}/add_entry/{id_entry}")
+async def add_entries(id_wiki: str, id_entry: str):
+    try:
+        print(id_wiki, id_entry)
+        response = await wiki_logic.add_entries(id_wiki, id_entry)
+        return response
+    except Exception as e:
+        print(f"Se produjo un error: {e}")  # Imprime el error para el diagnóstico
+        raise HTTPException(status_code=400, detail="Cannot create an entry")
