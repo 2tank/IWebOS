7794b636a9402fe0dd43ab228559c3516e9b401b
jaiRodRod <jaezro@uma.es>
2024-11-04 16:32:19 +0100

    json_schema_extra commentary

--- KiWiki_fastAPI/models/commentary_schema.py
+++ KiWiki_fastAPI/models/commentary_schema.py
@@ -11,3 +11,21 @@ class commentary(BaseModel):
     entryRating: Optional[int] = Field(None,ge=0,le=10) #La puntuacion que le da el usuario a la entrada del 0 al 10
     commentaryInReply: Optional[str] = None
     replies: Optional[List[str]] = []
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "user": "507f1f77bcf86cd799439011",
+                "entry": "507f1f77bcf86cd799439022",
+                "entry_version": "507f1f77bcf86cd799439033",
+                "content": "Este es un comentario de prueba.",
+                "date": "2024-11-02T15:23:52.461000",
+                "entryRating": 8,
+                "commentaryInReply": "507f1f77bcf86cd799439044",
+                "replies": [
+                    "507f1f77bcf86cd799439055",
+                    "507f1f77bcf86cd799439066"
+                ]
+            }
+        }
+    }
