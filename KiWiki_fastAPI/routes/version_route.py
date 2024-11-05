12328820a9a18d0aa391bc352bcea96cb2a39635
Antonio2233 <antoniosalvadorzafra@gmail.com>
2024-11-05 13:35:32 +0100

    excepciones version route

--- KiWiki_fastAPI/routes/version_route.py
+++ KiWiki_fastAPI/routes/version_route.py
@@ -11,7 +11,8 @@ async def get_versions():
         versions = await version_logic.get_versions()
         return versions
     except Exception as e:
-        raise HTTPException(status_code=500,  detail=f"Failed to retrieve versions: {str(e)}") 
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500,  detail=f"Failed to retrieve versions")
 
 @router.get("/{id}")
 async def get_version_by_id(id : str):
@@ -19,20 +20,32 @@ async def get_version_by_id(id : str):
         version = await version_logic.get_version_by_id(id)
         return version
     except Exception as e:
-        raise HTTPException(status_code=500,  detail=f"Failed to retrieve versions: {str(e)}") 
+        print({str(e)})
+        raise HTTPException(status_code=500, detail=f"Failed to retrieve version by ID")
 
 @router.put("/{id}")
 async def rollback_version_by_id(id : str):
+    try:
         version = await version_logic.rollback_version_by_id(id)
         return version
+    except Exception as e:
+        print({str(e)})
+        raise HTTPException(status_code=500, detail=f"Failed to rollback version by ID")
 
 @router.delete("/{id}")
 async def delete_version_by_id(id : str):
+    try:
         deleted_version = await version_logic.delete_version_by_id(id)
         return deleted_version
-
+    except Exception as e:
+        print({str(e)})
+        raise HTTPException(status_code=500, detail=f"Failed to delete version by ID")
 
 @router.get("/{id}")
 async def get_entry_by_version_id(id : str):
+    try:
         entry = await version_logic.delete_version_by_id(id)
         return entry
+    except Exception as e:
+        print({str(e)})
+        raise HTTPException(status_code=500, detail=f"Failed to retrieve entry by Version ID")
