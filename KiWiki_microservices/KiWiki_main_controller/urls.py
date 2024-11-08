013c3c3eafeaff2681090679df978d70da1b3014
Zoe <124881400+ZocufyZoe@users.noreply.github.com>
2024-11-08 23:59:43 +0100

    url notification

--- KiWiki_microservices/KiWiki_main_controller/urls.py
+++ KiWiki_microservices/KiWiki_main_controller/urls.py
@@ -2,5 +2,5 @@ config = {
     "wiki_url": "http://127.0.0.1:8001/wikis",
     "entry_url": "http://api.mientrada.com",
     "commentary_url": "http://api.miversion.com",
-    "notification_url": "http://api.minotificacion.com"
+    "notification_url": "http://127.0.0.1:8004/notification"
 }
