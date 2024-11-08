2a5a7efdf2180212c5723157420a15f605cbee3f
jaiRodRod <jaezro@uma.es>
2024-11-08 19:21:30 +0100

    MICROSERVICIOS

---
+++ KiWiki_fastAPI/item_logic/version.py
@@ -0,0 +1,85 @@
+from fastapi.encoders import jsonable_encoder
+from item_logic.crud_inheritance.version_crud import VersionCRUD
+from item_logic.crud_inheritance.entry_crud import ENTRYCRUD
+
+
+entry_crud = ENTRYCRUD()
+crud = VersionCRUD()
+
+async def get_versions(filter):
+
+    if(len(filter) > 0):
+        versions = await crud.get_by_filter(filter)
+    else:
+        versions = await crud.get_collection()
+
+    return versions
+
+async def get_version_by_id(id):
+    version = await crud.get_id(id)
+    return version
+
+async def get_versions_by_entryid(entry_id):
+    versions = await crud.get_versions_by_entryid(entry_id)
+    return versions
+
+async def get_entry_by_version_id(id):
+    version = await crud.get_id(id)
+    entry_id = version['entry_id']
+
+    #Entrada referenciada
+    entry = await entry_crud.get_id(entry_id)
+
+    return entry
+
+
+async def rollback_version_by_id(id):
+    version = await crud.get_id(id)
+    entry_id = version['entry_id']
+
+    #Entrada referenciada
+    entry = await entry_crud.get_id(entry_id)
+
+    actualVersionID = entry["actual_version"]
+    deleted_version = None
+
+    if ( actualVersionID == id ) :
+        versions = await crud.get_versions_by_entryid(entry_id,reverted=True)
+        numVersions = len(versions)
+
+        if (numVersions > 1):
+            newId = versions[1]["_id"]
+
+            #actualizar referencia a nueva version actual
+            await entry_crud.update_id(entry_id, {"actual_version": newId})
+            await crud.update_id(version["_id"],{"reverted": True})
+    return deleted_version
+
+
+async def delete_version_by_id(id):
+    version = await crud.get_id(id)
+    entry_id = version['entry_id']
+
+    #Entrada referenciada
+    entry = await entry_crud.get_id(entry_id)
+
+    actualVersionID = entry["actual_version"]
+    deleted_version = None
+
+    if ( actualVersionID != id ) : # borra si no es actual
+        deleted_version = await crud.delete_id(id)
+
+    return deleted_version
+
+
+# rollback manual (elige que version pasa a ser la actual)
+async def update_actual_version_by_id(entry_id,version_id):
+    versions = await crud.get_versions_by_entryid(entry_id,reverted=False)
+
+    if any(str(v["_id"]) == version_id for v in versions):
+        await entry_crud.update_id(entry_id, {"actual_version": version_id})
+        await crud.update_id(version_id, {"reverted": False})
+    else:
+        return False
+
+    return True
---
+++ KiWiki_microservices/KiWiki_entry/models/entry_schema.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+from datetime import datetime, timezone, timedelta
+from typing import List, Literal
+
+#No tengo claro si dejar los tags como literales internos o pasarlo a una tabla de la bbdd
+#por ahora lo dejo aqui por simplicidad y tambien pienso que será algo predefinido
+#y que se editará con muy poca frecuencia por lo que no lo veo mal aquí.
+entryType = Literal['POLITICS', 'SPORTS', 'CINEMA']  # A expandir...
+
+class entrySchema(BaseModel):
+    title: str = Field(..., max_length=20, description="Titulo de la Entrada")
+    creator : str = Field(..., max_length=20, description="Creador de la Entrada")
+    creationDate: datetime = Field(default_factory=lambda:datetime.now(timezone(timedelta(hours=2))) ,description="Fecha creación de la Entrada")
+    description: str = Field(...,max_length=50, description="Descripción de la entrada")
+    tags: List[entryType] = Field(default_factory=list,description="Tags asociados a la entrada")
+    actual_version: str = None
+
+    model_config = {
+        "json_schema_extra" : {
+            "example" : 
+            {
+                "_id": "67264426ae903056cd2d7ac7",
+                "title": "Entrada Prueba",
+                "creator": "Creador Prueba",
+                "creationDate": "2024-11-02T15:23:52.461000",
+                "description": "Descripcion Prueba",
+                "tags": [
+                    "POLITICS"
+                ],
+                "actual_version": ""
+            }
+        }
+    }
