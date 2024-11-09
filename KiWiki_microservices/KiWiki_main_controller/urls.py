f132dd74f143f68ffa2065c2a8aaa2ca54987e55
jaiRodRod <jaezro@uma.es>
2024-11-09 16:34:39 +0100

    Comentarios comentariados

--- KiWiki_microservices/KiWiki_main_controller/urls.py
+++ KiWiki_microservices/KiWiki_main_controller/urls.py
@@ -1,7 +1,7 @@
 config = {
     "wiki_url": "http://127.0.0.1:8001/wikis",
     "entry_url": "http://127.0.0.1:8002/entries",
-    "commentary_url": "http://api.miversion.com",
+    "commentary_url": "http://127.0.0.1:8003/commentaries",
     "notification_url": "http://127.0.0.1:8004/notification",
     "version_url": "http://127.0.0.1:8005/versions"
 }
