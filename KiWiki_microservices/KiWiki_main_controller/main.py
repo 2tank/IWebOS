27b983c40cb0b8a8d33aeb16c05a6683dba7a00d
Zoe <124881400+ZocufyZoe@users.noreply.github.com>
2024-11-10 14:32:36 +0100

    ruta notification incluida

--- KiWiki_microservices/KiWiki_main_controller/main.py
+++ KiWiki_microservices/KiWiki_main_controller/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import wiki_route,entry_route,version_route, commentary_route
+from routes import wiki_route,entry_route,version_route, commentary_route, notification_route
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app.include_router(entry_route.router, prefix="/entries")
 app.include_router(wiki_route.router, prefix="/wikis")
 app.include_router(version_route.router,prefix="/versions")
 app.include_router(commentary_route.router,prefix="/commentaries")
+app.include_router(notification_route.router,prefix="/notification")
