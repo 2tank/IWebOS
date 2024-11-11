cd55aaad474a8ab69bb0b1943492138877d5ebda
Adrián Fernández Vera <adrianfeve7@gmail.com>
2024-11-11 18:58:28 +0100

    router creado ;)

--- KiWiki_frontend/KiWiki/src/App.jsx
+++ KiWiki_frontend/KiWiki/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
 
   return (
 
-
-
     <div>
       <h1>Bienvenido a KiWiki, cabrón</h1>
     </div>
--- KiWiki_frontend/KiWiki/src/main.jsx
+++ KiWiki_frontend/KiWiki/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} /> {/* Ruta para 404 */}
+      </Routes>
+    </Router>
 )
