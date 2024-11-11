f7c546f181334df09c10b91b8280afb5cdfc517c
Adrián Fernández Vera <adrianfeve7@gmail.com>
2024-11-11 18:52:31 +0100

    React router dom instaldos

--- KiWiki_frontend/KiWiki/tailwind.config.js
+++ KiWiki_frontend/KiWiki/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Indica que Tailwind escanee archivos en `src`
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
