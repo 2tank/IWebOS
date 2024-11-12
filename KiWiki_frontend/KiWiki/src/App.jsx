bb753672579cb8813c320c6b33cb380cd80f14c9
Adrián Fernández Vera <adrianfeve7@gmail.com>
2024-11-12 10:01:24 +0100

    Añadido de logo y un par de lineas (a mejorar)

--- KiWiki_frontend/KiWiki/src/App.jsx
+++ KiWiki_frontend/KiWiki/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import logoWiki from '../public/assets/logo_wiki.png'
+
 
 function App() {
 
   return (
 
-    <div>
+    <div className='max-h-screen flex-row justify-center items-center max-h-screen bg-white'>
+      <img src={logoWiki} className='flex-1'></img>
+      <div className='flex-1'>
       <h1>Bienvenido a KiWiki, cabrón</h1>
+      </div>
     </div>
 
   )
