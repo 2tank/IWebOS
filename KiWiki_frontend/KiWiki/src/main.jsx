f5d9238e7af2e3912b0be16ae768a7ff118f546d
jaiRodRod <jaezro@uma.es>
2024-11-25 21:14:33 +0100

    Porfa dejadme el entryTest para los comentarios

--- KiWiki_frontend/KiWiki/src/main.jsx
+++ KiWiki_frontend/KiWiki/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import HomePage from './HomePage/Components/HomePage.jsx';
 import EntrySection from './Entry/EntrySection.jsx';
 import Wiki from './Wiki/Wiki.jsx';
+import EntryTest from './Entry/EntryTest.jsx';
 createRoot(document.getElementById('root')).render(
 
   <Router>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
         {/* <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} /> {/* Ruta para 404 */}
         <Route path='/wikis/:selectedOpition/:query' element={<Wiki/>}></Route>
+        <Route path='/entryTest' element={<EntryTest/>} /> {/*DEJADME ESTO PORFA, voy a ensuciar esto para los comentarios y cuando esten listo los exporto a la entry*/}
       </Routes>
     </Router>
 )
