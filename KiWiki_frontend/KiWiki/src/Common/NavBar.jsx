5f06e08fa2f95092d89efa367f82efa48ef91fc7
Zoe <124881400+ZocufyZoe@users.noreply.github.com>
2024-12-22 21:01:56 +0100

    Login hecho sin accesos a bd

--- KiWiki_frontend/KiWiki/src/Common/NavBar.jsx
+++ KiWiki_frontend/KiWiki/src/Common/NavBar.jsx
@@ -1,41 +1,47 @@
 import { Link, useLocation } from 'react-router-dom'; 
 import SearchBar from './SearchBar'; 
 import Avatar from '@mui/material/Avatar';
-import Notification from '../Common/Notification'
-import {useNotification} from './NotificationContext'
+import Notification from '../Common/Notification';
+import { useNotification } from './NotificationContext';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from '../Login/LoginButton';
+import LogoutButton from '../Login/LogoutButton';
 
 const Navbar = () => {
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+
   const location = useLocation();
   const shouldShowSearchBar = location.pathname !== "/";
 
-  const {unreadCount} = useNotification();
+  const { unreadCount } = useNotification();
 
   return (
     <nav className="w-full flex flex-col sm:flex-row items-center justify-between p-4 bg-amber-950 text-white">
       <div className="flex items-center">
         <Link to="/" className="hover:opacity-80">
-            <Avatar sx={{ width: 80, height: 80 }} src='/assets/logo_wiki_blanco.png'/>
+          <Avatar className="w-20 h-20 sm:w-16 sm:h-16" src='/assets/logo_wiki_blanco.png' />
         </Link>
       </div>
 
       <div className="flex-grow flex justify-center mb-2 sm:mb-0">
-        {shouldShowSearchBar && <SearchBar/>}
+        {shouldShowSearchBar && <SearchBar />}
       </div>
 
-      <div className='mt-2 sm:mt-0 flex flex-row'>
-
-      <Link className='row' to="/notifications">
-        <Notification unreadCount={unreadCount} />
-      </Link>
-
-      <Link to="/" className="hover:opacity-80 flex flex-col items-center row">
-            
-            <Avatar src='/src/assets/image.png'/>
-            
-            <span className="text-center">
-                Iniciar Sesión
-            </span>
+      <div className="mt-2 sm:mt-0 flex flex-row items-center space-x-4">
+        <Link className="row" to="/notifications">
+          <Notification unreadCount={unreadCount} />
         </Link>
+
+        <div className="flex items-center space-x-2">
+        {!isAuthenticated ? (
+            <LoginButton />
+        ) : (
+            <>
+            <Avatar src={user?.picture} alt={user?.name} />
+            <LogoutButton />
+            </>
+        )}
+        </div>
       </div>
     </nav>
   );
--- KiWiki_frontend/KiWiki/src/main.jsx
+++ KiWiki_frontend/KiWiki/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import HomePage from './HomePage/Components/HomePage.jsx';
 import DetailedEntry from './Entry/DetailedEntry.jsx';
 import Wiki from './Wiki/Wiki.jsx';
@@ -14,24 +16,31 @@ import NotificationProvider from './Common/NotificationContext.jsx';
 import NotificationPage from './Notifications/NotificationPage.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <NotificationProvider>
-    <Router>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/wikis/:nameWiki/entries/:nameEntry" element={<DetailedEntry/>} />
-            <Route path="/wikis/:nameWiki/entries/:entry_id/versionedit" element={<PostVersion/>}/> 
-            <Route path='/wikis/:nameWiki/entries' element={<ListEntries/>}></Route>
-            <Route path='/wikis/:wiki_id/create' element={<PostEntry/>}></Route>
-            <Route path='/wikis/:nameWiki/entries/:entry_id/modify' element={<PostEntry/>}></Route>
-            <Route path='/wikis/:selectedOption/:query/:dateOption' element={<Wiki/>}></Route>
-            <Route path='/wikis/:selectedOption/:query' element={<Wiki/>}></Route>
-            <Route path='/wikis/create' element={<CreateWiki/>}></Route>
-            <Route path='/wikis/:nameWiki/modify' element={<CreateWiki/>}></Route>
-            <Route path="/notifications" element={<NotificationPage/>} />
-            {/* <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} /> {/* Ruta para 404 */}
-          </Routes>
-    </Router>
-  </NotificationProvider>
-  
+  <Auth0Provider
+    domain="dev-othpjtkbmz853lr2.us.auth0.com"
+    clientId="n6g8BGEoNRD8Zxc0HI2s0BtUGFGatA9M"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <NotificationProvider>
+      <Router>
+            <Routes>
+              <Route path="/" element={<HomePage/>} />
+              <Route path="/wikis/:nameWiki/entries/:nameEntry" element={<DetailedEntry/>} />
+              <Route path="/wikis/:nameWiki/entries/:entry_id/versionedit" element={<PostVersion/>}/> 
+              <Route path='/wikis/:nameWiki/entries' element={<ListEntries/>}></Route>
+              <Route path='/wikis/:wiki_id/create' element={<PostEntry/>}></Route>
+              <Route path='/wikis/:nameWiki/entries/:entry_id/modify' element={<PostEntry/>}></Route>
+              <Route path='/wikis/:selectedOption/:query/:dateOption' element={<Wiki/>}></Route>
+              <Route path='/wikis/:selectedOption/:query' element={<Wiki/>}></Route>
+              <Route path='/wikis/create' element={<CreateWiki/>}></Route>
+              <Route path='/wikis/:nameWiki/modify' element={<CreateWiki/>}></Route>
+              <Route path="/notifications" element={<NotificationPage/>} />
+              {/* <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} /> {/* Ruta para 404 */}
+            </Routes>
+      </Router>
+    </NotificationProvider>
+  </Auth0Provider>
 )
