60eef4b700b85907d03aebcc3f3664cb71b22d34
jaiRodRod <jaezro@uma.es>
2024-12-28 12:40:19 +0100

    Session avances, antes de comer, ahora sigo

--- KiWiki_frontend/KiWiki/src/Common/SessionProvider.jsx
+++ KiWiki_frontend/KiWiki/src/Common/SessionProvider.jsx
@@ -1,15 +1,25 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import axios from 'axios'
 
 const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false); 
-  const [ sessionProfile, setSessionProfile ] = useState(null);
+  const [sessionProfile, setSessionProfile ] = useState(null);
 
+  const [urlUser, setUrlUser] = useState(null);
+  const [data, setData] = useState(null);
+  const [done, setDone] = useState(false);
+  const [user, setUser] = useState(false);
 
   const funLogin = (profile) => {
     setIsLoggedIn(true); 
     setSessionProfile(profile);
+
+
+    setUrlUser(`http://localhost:8000/users/${profile.email}`);
+
+
   };
 
   const funLogout = () => {
@@ -17,8 +27,58 @@ export const SessionProvider = ({ children }) => {
     setSessionProfile(null);
   };
 
+  //Tratar de obtener el user por email
+  useEffect(() => {
+    const fetchData = async() => {
+          try {
+            if(urlUser) {
+                const response = await axios.get(urlUser);
+                setData(response.data);
+            }
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+          setDone(true);
+        }
+    };
+    fetchData();
+  }, [urlUser]);
+
+  //Si ha encontrado un user por email lo pone, si no hace una peticion para postearlo con permisos basicos
+  useEffect(() => {
+    const postUser = async() => {
+    
+        //REDACTOR, EDITOR, ADMIN
+
+        const payload = {
+            rol: 'REDACTOR',
+            email: sessionProfile.email,
+            sendEmail: false,
+        };
+    
+        try {
+          const response = await axios.post('http://localhost:8000/users/', payload, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+        } catch (error) {
+          console.error("Error posting data:", error);
+        } finally {
+          
+        }
+    };
+    if(done && data !== null) {
+      //Guardar en la sesion el user
+      setUser(data);
+    } else if (done && sessionProfile !== null) {
+      //Meter un user en la bd
+      postUser();
+    }
+  },[done, data]);
+
   return (
-    <SessionContext.Provider value={{ isLoggedIn, sessionProfile, funLogin, funLogout }}>
+    <SessionContext.Provider value={{ isLoggedIn, sessionProfile, user, funLogin, funLogout }}>
       {children}
     </SessionContext.Provider>
   );
